0f0aa89b18a5e7ea1257f6d06f48aeaaba5fb433
unknown <omar.er3434@gmail.com>
2025-03-13 22:51:07 +0200

    Add Footer Section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import footerCSS from './footer.module.css';
+import { motion } from 'framer-motion';
+import Animations from '../../Animations/Animations';
 
 export default function Footer() {
 
@@ -9,11 +11,15 @@ export default function Footer() {
 
     return <React.Fragment>
 
-        <footer className={`parents_cont ${footerCSS.container}`}>
+        <motion.footer 
+            variants={Animations.parentScaleVariants} 
+            initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.3 }}
+            className={`parents_cont ${footerCSS.container}`}
+        >
 
             <p>© 2025 <span>{t('footerName')}</span>. {t('footerMsg')}.</p>
 
-        </footer>
+        </motion.footer>
 
     </React.Fragment>
 
