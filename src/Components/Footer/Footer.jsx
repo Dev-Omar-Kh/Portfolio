21b3bdf0ca6b08ba9a49f28c1aba89c3a5a54cab
unknown <omar.er3434@gmail.com>
2025-03-13 17:23:23 +0200

    Add Footer Section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import footerCSS from './footer.module.css';
 
 export default function Footer() {
 
+    const {t} = useTranslation();
+
     return <React.Fragment>
 
         <footer className={`parents_cont ${footerCSS.container}`}>
 
-            <p>© 2025 <span>Omar Khaled</span>. All Rights Reserved.</p>
+            <p>© 2025 <span>{t('footerName')}</span>. {t('footerMsg')}.</p>
 
         </footer>
 
